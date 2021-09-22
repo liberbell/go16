4a89e6b4a394f20b20b25e6d8c381d607cf25d7a
Liberbell <liberbell@gmail.com>
2021-09-22 09:15:01 +0900

    main2

--- errorhandle/main.go
+++ errorhandle/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func main() {
 
 	// signature: func open
 	f, err := os.Open("myFile.txt")
+	defer f.Close()
+
+	if err != nil {
+		log.Println(err)
+	}
 }

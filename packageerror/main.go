0069a47fd9567a43d783a0b79ce070b8f8eed2c7
Liberbell <liberbell@gmail.com>
2021-09-22 09:25:21 +0900

    main3

--- packageerror/main.go
+++ packageerror/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"errors"
+	"log"
 	"os"
 )
 
 func main() {
 	a
 	if _, err := os.Open("myFile.txt"); err != nil {
-		if errors.Is(err, os.ErrNotExist)
+		if errors.Is(err, os.ErrNotExist) {
+			log.Println(File does not exist.)
+		}
 	}
 }

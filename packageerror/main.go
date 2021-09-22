c14656174cea10b8d8fcf14f96580cd27ba6d7fc
Liberbell <liberbell@gmail.com>
2021-09-22 09:26:48 +0900

    main6

--- packageerror/main.go
+++ packageerror/main.go
@@ -11,6 +11,8 @@ func main() {
 	if _, err := os.Open("myFile.txt"); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			log.Println("File does not exist.")
+		} else {
+			log.Println(err)
 		}
 	}
 }

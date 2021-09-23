72d5d762264636061cdcdd2d2c6da1fbdbb204a6
Liberbell <liberbell@gmail.com>
2021-09-23 13:11:17 +0900

    main3

--- panicrecover/main.go
+++ panicrecover/main.go
@@ -1,11 +1,18 @@
 package main
 
+import "fmt"
+
 func p(s string, i int) {
 	if i > 2 {
 		panic(s)
 	}
 }
 
+func r() {
+	if err := recover(); err != nil {
+		fmt.Println(err)
+	}
+}
 func main() {
 	a
 }

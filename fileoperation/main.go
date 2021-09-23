1a747317c98cf23abded8772d86745dc324c5a57
Liberbell <liberbell@gmail.com>
2021-09-24 08:43:57 +0900

    main5

--- fileoperation/main.go
+++ fileoperation/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
+//Open and Close file
+func closer(f *os.File) error {
+	f.Close()
+	fmt.Println(f.Name(), " successfully closed.")
+}
+
 func main() {
 	//Create File
-	f, err := os.Create("create.txt")
-	defer f.Close()
+	// f, err := os.Create("create.txt")
+	// defer f.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(f)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(f)
+
+	//Open and Cloes file
+
 }

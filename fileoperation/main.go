5ca510d86f768ad62ccf67d3112a5797255275cb
Liberbell <liberbell@gmail.com>
2021-09-24 08:39:42 +0900

    main2

--- fileoperation/main.go
+++ fileoperation/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	f, err := os.Open("create.txt")
+	defer f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(f)
 }

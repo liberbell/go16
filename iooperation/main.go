cda05ca85c15cd20457ad6588971387b3093f2bb
Liberbell <liberbell@gmail.com>
2021-09-26 11:15:49 +0900

    main7

--- iooperation/main.go
+++ iooperation/main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 
 	//Make directory
-	d, err := os.Stat("subdir")
-	fmt.Println("error returned by os.Stat is: ", err)
+	// d, err := os.Stat("subdir")
+	// fmt.Println("error returned by os.Stat is: ", err)
 
-	if err == nil {
-		fmt.Println(d.Mode(), d.IsDir())
-		log.Fatal("file/directory name already exists.")
-	}
-	if errors.Is(err, os.ErrNotExist) {
-		err := os.MkdirAll("subdir", 0777)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("subdir directory created.")
-	}
+	// if err == nil {
+	// 	fmt.Println(d.Mode(), d.IsDir())
+	// 	log.Fatal("file/directory name already exists.")
+	// }
+	// if errors.Is(err, os.ErrNotExist) {
+	// 	err := os.MkdirAll("subdir", 0777)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println("subdir directory created.")
+	// }
+
+	//Make directory all
+	p := filepath.Join("../test", "subdir1", "subdir2")
+	err := os.MkdirAll(p, 0777)
 }
